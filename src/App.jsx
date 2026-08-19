b396b441ca9522db5dda7c313f10e39f6b83c211
Humberto Soriano <humbertosorianotrujillo@gmail.com>
2026-08-19 10:18:47 +0000

    External agent changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Home from '@/pages/Home';
 import Buscar from '@/pages/Buscar';
 import Parte from '@/pages/Parte';
+import Comparar from '@/pages/Comparar';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import ForgotPassword from '@/pages/ForgotPassword';
@@ -52,6 +53,7 @@ const AuthenticatedApp = () => {
         <Route path="/" element={<Home />} />
         <Route path="/buscar" element={<Buscar />} />
         <Route path="/parte/:id" element={<Parte />} />
+        <Route path="/comparar/:id" element={<Comparar />} />
       </Route>
       <Route path="*" element={<PageNotFound />} />
     </Routes>
