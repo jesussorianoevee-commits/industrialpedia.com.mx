bd3197ed55d13a1804b125ff39114b4fd37f7089
Humberto Soriano <humbertosorianotrujillo@gmail.com>
2026-08-23 03:17:56 +0000

    External agent changes

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -8,10 +8,15 @@ const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
-/** @param {React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>} props */
-const Label = React.forwardRef(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-))
+/**
+ * @param {React.ComponentPropsWithoutRef<'label'>} props
+ * @param {React.ForwardedRef<HTMLLabelElement>} ref
+ */
+function LabelComponent({ className, ...props }, ref) {
+  return <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />;
+}
+
+const Label = React.forwardRef(LabelComponent)
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
