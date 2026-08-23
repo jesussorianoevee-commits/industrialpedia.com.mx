75d533505b48b22b673255be1f96b49b87a51432
Humberto Soriano <humbertosorianotrujillo@gmail.com>
2026-08-23 03:14:47 +0000

    External agent changes

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+/** @param {React.ComponentPropsWithoutRef<'input'>} props */
 const Input = React.forwardRef(({ className, type, ...props }, ref) => {
   return (
     (<input
